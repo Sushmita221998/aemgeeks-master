639ecbf0132b56364f5c21e042fe1ba00639020e
Sushmita221998 <sushmitaghatge98@gmail.com>
2026-02-03 22:35:32 +0530

    Simple and nested multifield dialog changes

--- core/src/main/java/com/aem/geeks/core/models/Hobbies.java
+++ core/src/main/java/com/aem/geeks/core/models/Hobbies.java
@@ -1,26 +1,19 @@
 package com.aem.geeks.core.models;
 
 import javax.inject.Inject;
-import org.apache.sling.api.resource.Resource;
 
+import org.apache.sling.api.resource.Resource;
 import org.apache.sling.models.annotations.DefaultInjectionStrategy;
 import org.apache.sling.models.annotations.Model;
 
 @Model(adaptables = Resource.class, defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
-public class Test1Model {
+public class Hobbies {
 
 	@Inject
-	private String id;
+	private String hobbies;
 
-	@Inject
-	private String contact;
-
-	public String getId() {
-		return id;
-	}
-
-	public String getContact() {
-		return contact;
+	public String getHobbies() {
+		return hobbies;
 	}
 
 }
---
+++ core/src/main/java/com/aem/geeks/core/models/Test2Model.java
@@ -0,0 +1,80 @@
+package com.aem.geeks.core.models;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.inject.Inject;
+
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.models.annotations.DefaultInjectionStrategy;
+import org.apache.sling.models.annotations.Model;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Model(adaptables = { Resource.class }, defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
+public class Test2Model {
+
+	@Inject
+	private Resource resource;
+
+	private String MULTIFIELD_CHILD_NODE_NAME = "multifieldHobbiesSection";
+	private static final Logger LOG = LoggerFactory.getLogger(Test2Model.class);
+
+	@Inject
+	private String id;
+
+	@Inject
+	private String name;
+
+	@Inject
+	private String address;
+
+	// NEW APPROACH STARTS HERE ---
+	// @ChildResource(name="multifieldHobbiesSection")
+	// NEW APPROACH ENDS HERE ---
+	private List<Hobbies> hobbiesList;
+
+	// OLD APPROACH STARTS HERE ---
+
+	@PostConstruct
+	protected void init() {
+		LOG.info("Inside init method");
+		if ((resource != null) && (resource.getChild(MULTIFIELD_CHILD_NODE_NAME) != null)) {
+			hobbiesList = new ArrayList<Hobbies>();
+			Resource linkRootRes = resource.getChild(MULTIFIELD_CHILD_NODE_NAME);
+			Iterable<Resource> resItr = linkRootRes.getChildren();
+			for (Resource res : resItr) {
+				Hobbies hobbies = res.adaptTo(Hobbies.class);
+				createHobbiesList(hobbies, hobbiesList, resource);
+			}
+		}
+	}
+
+	private void createHobbiesList(Hobbies hobbies, List<Hobbies> hobbiesList, Resource res) {
+		if (hobbies != null) {
+			hobbiesList.add(hobbies);
+		}
+		LOG.info("List Size----" + hobbiesList.size());
+
+	}
+
+	// OLD APPROACH ENDS HERE ---
+
+	public String getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public List<Hobbies> getHobbiesList() {
+		return hobbiesList;
+	}
+
+}
