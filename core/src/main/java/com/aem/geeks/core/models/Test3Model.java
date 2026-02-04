d4c3a5d71bfb59c0f127c815903c353d5d0d4a8a
Sushmita221998 <sushmitaghatge98@gmail.com>
2026-02-04 22:08:25 +0530

    Fetch Multifield dialog values using Resource and SlingHttpServletRequest Class

--- core/src/main/java/com/aem/geeks/core/models/Test3Model.java
+++ core/src/main/java/com/aem/geeks/core/models/Test3Model.java
@@ -6,20 +6,21 @@ import java.util.List;
 import javax.annotation.PostConstruct;
 import javax.inject.Inject;
 
+import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.resource.Resource;
 import org.apache.sling.models.annotations.DefaultInjectionStrategy;
 import org.apache.sling.models.annotations.Model;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-@Model(adaptables = { Resource.class }, defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
-public class Test2Model {
+@Model(adaptables = { SlingHttpServletRequest.class }, defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
+public class Test3Model {
 
 	@Inject
-	private Resource resource;
+	private SlingHttpServletRequest request;
 
 	private String MULTIFIELD_CHILD_NODE_NAME = "multifieldHobbiesSection";
-	private static final Logger LOG = LoggerFactory.getLogger(Test2Model.class);
+	private static final Logger LOG = LoggerFactory.getLogger(Test3Model.class);
 
 	@Inject
 	private String id;
@@ -35,23 +36,23 @@ public class Test2Model {
 	// NEW APPROACH ENDS HERE ---
 	private List<Hobbies> hobbiesList;
 
-	// OLD APPROACH STARTS HERE --- USING RESOURCE
+	// OLD APPROACH STARTS HERE --- USING SlingHttpServletRequest
 
 	@PostConstruct
 	protected void init() {
 		LOG.info("Inside init method");
-		if ((resource != null) && (resource.getChild(MULTIFIELD_CHILD_NODE_NAME) != null)) {
+		if ((request != null) && (request.getResource() != null) && (request.getResource().getChild(MULTIFIELD_CHILD_NODE_NAME) != null)) {
 			hobbiesList = new ArrayList<Hobbies>();
-			Resource linkRootRes = resource.getChild(MULTIFIELD_CHILD_NODE_NAME);
+			Resource linkRootRes = request.getResource().getChild(MULTIFIELD_CHILD_NODE_NAME);
 			Iterable<Resource> resItr = linkRootRes.getChildren();
 			for (Resource res : resItr) {
 				Hobbies hobbies = res.adaptTo(Hobbies.class);
-				createHobbiesList(hobbies, hobbiesList, resource);
+				createHobbiesList(hobbies, hobbiesList);
 			}
 		}
 	}
 
-	private void createHobbiesList(Hobbies hobbies, List<Hobbies> hobbiesList, Resource res) {
+	private void createHobbiesList(Hobbies hobbies, List<Hobbies> hobbiesList) {
 		if (hobbies != null) {
 			hobbiesList.add(hobbies);
 		}
